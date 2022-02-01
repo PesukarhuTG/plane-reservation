809a47fb7eb3f72149110a40c423d11d9af65a4e
Tatiana <ganzhinovat@yandex.ru>
2022-02-01 12:19:31 +0300

    refactor: change code

--- modules/start.js
+++ modules/start.js
@@ -1,7 +1,14 @@
-const start = (app, title) => {
+const createTitle = (title) => {
   const h1 = document.createElement('h1');
   h1.className = 'title';
   h1.textContent = title;
+
+  return h1;
+}
+
+
+const start = (app, title) => {
+  const h1 = createTitle(title);
 
   app.append(h1);
 };
