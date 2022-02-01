60c7f23271c185a62b4643e7059cebd895bc239e
Tatiana <ganzhinovat@yandex.ru>
2022-02-01 12:32:05 +0300

    feat: implement createElement function

--- modules/start.js
+++ modules/start.js
@@ -1,16 +1,27 @@
+import createElement from "./createElement.js";
+
 const createTitle = (title) => {
-  const h1 = document.createElement('h1');
-  h1.className = 'title';
-  h1.textContent = title;
+  const h1 = createElement('h1', {
+    className: 'title',
+    textContent: title,
+  });
 
   return h1;
 }
 
+const createMain = () => {
+  const main = createElement('main', {
+    className: 'person-data',
+  });
+
+  return main;
+}
 
 const start = (app, title) => {
   const h1 = createTitle(title);
+  const main = createMain();
 
-  app.append(h1);
+  app.append(h1, main);
 };
 
 export default start;
