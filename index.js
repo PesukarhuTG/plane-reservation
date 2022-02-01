4b0a867e991cad5c119889ea71913c672e6a4ac1
Tatiana <ganzhinovat@yandex.ru>
2022-02-01 12:17:08 +0300

    feat: add title

---
+++ index.js
@@ -0,0 +1,8 @@
+import start from "./modules/start.js";
+
+const init = (selectorApp, title) => {
+  const app = document.querySelector(selectorApp);
+  start(app, title);
+};
+
+init('.app', 'Выберите тур');
---
+++ modules/start.js
@@ -0,0 +1,9 @@
+const start = (app, title) => {
+  const h1 = document.createElement('h1');
+  h1.className = 'title';
+  h1.textContent = title;
+
+  app.append(h1);
+};
+
+export default start;
