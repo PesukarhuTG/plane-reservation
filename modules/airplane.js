e962ca898a98374c90e321a722f44908b8e179d6
Tatiana <ganzhinovat@yandex.ru>
2022-02-02 14:48:52 +0300

    feat: add fuselage + seats

--- modules/airplane.js
+++ modules/airplane.js
@@ -28,6 +28,43 @@ const createExit = () => {
   return fuselage;
 }
 
+const createSeatBlock = (n, count) => {
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+  const fuselage = createElement('ol', {
+    className: 'fuselage',
+  });
+
+  for (let i = n; i < count + n; i++) {
+    const rowWrapper = createElement('li');
+    const seats = createElement('ol', {
+      className: 'seats',
+    });
+
+    const seatsRow = letters.map(letter => {
+      const seat = createElement('li', {
+        className: 'seat',
+      })
+
+      const checkWrapper = createElement('label');
+      const check = createElement('input', {
+        name: 'seat',
+        type: 'checkbox',
+        value: `${i}${letter}`,
+      });
+
+      checkWrapper.append(check);
+      seat.append(checkWrapper);
+      return seat;
+    });
+
+    seats.append(...seatsRow);
+    rowWrapper.append(seats);
+    fuselage.append(rowWrapper);
+  }
+  return fuselage;
+}
+
 const createAirplane = (title, scheme) => {
   const choisesSeat = createElement('form', {
     className: 'choises-seat',
@@ -40,9 +77,15 @@ const createAirplane = (title, scheme) => {
 
   const cockpit = createCockpit(title);
 
+  let n = 1; //отчет кол-ва мест в ряду
   //создание мест из массива схемы самолета
   const elements = scheme.map(typeElem => {
     if (typeElem === 'exit') return createExit();
+    if (typeof typeElem === 'number') {
+      const seatBlock = createSeatBlock(n, typeElem);
+      n += typeElem; //кресла с 1...11, потом с 12...23
+      return seatBlock;
+    }
   });
 
   plane.append(cockpit, ...elements);
