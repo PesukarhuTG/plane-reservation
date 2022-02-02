d6f6290d7348c15c28c6a230b31253974fccdc1e
Tatiana <ganzhinovat@yandex.ru>
2022-02-02 12:04:20 +0300

    feat: implement save forms data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import start from "./modules/start.js";
 import getFormPerson from "./modules/formPerson.js";
+import readyPlane from "./modules/readyPlane.js";
 
 const init = (selectorApp, title) => {
   const app = document.querySelector(selectorApp);
@@ -14,6 +15,8 @@ const init = (selectorApp, title) => {
     //создаем новые формы в зависимости от кол-ва указанных человек
     const forms = getFormPerson(firstForm.count.value);
     main.append(...forms);
+
+    readyPlane(forms, main);
   });
 };
 
---
+++ modules/readyPlane.js
@@ -0,0 +1,28 @@
+const readyPlane = (forms, main) => {
+  const data = [];
+
+  forms.forEach(form => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+
+      //каждую заполняемую форму блокируем после ввода данных
+      for (const element of form.elements) {
+        element.disabled = true;
+      }
+
+      //сохраняем вводимые данные пассажиров
+      data.push({
+        name: form.name.value,
+        ticket: form.ticket.value,
+      });
+
+      //когда все формы заполнятся, скрываем формы и запускаем показ самолета
+      if (forms.length === data.length) {
+        forms.forEach(form => form.remove());
+        airplane(main, data);
+      }
+    });
+  })
+}
+
+export default readyPlane;
