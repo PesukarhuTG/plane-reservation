4d752d1069846acbe860ff1c389986808e7a1e6b
Tatiana <ganzhinovat@yandex.ru>
2022-02-02 12:09:08 +0300

    refactor: add description to index.js

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const init = (selectorApp, title) => {
   const app = document.querySelector(selectorApp);
   const { main, firstForm } = start(app, title);
 
+  //когда заполнили первую форму с кол-вом пассажиров...
   firstForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    //а первая форма удаляется со страницы
+    //удаляем первую форму со страницы...
     firstForm.remove();
 
-    //создаем новые формы в зависимости от кол-ва указанных человек
+    //создаем новые формы в зависимости от кол-ва человек и показываем на странице
     const forms = getFormPerson(firstForm.count.value);
     main.append(...forms);
 
+    //запускаем проверку заполнения форм для дальнейшего показа замолета
     readyPlane(forms, main);
   });
 };
