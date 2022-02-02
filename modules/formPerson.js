0c39c8c23e762ea3e88344a69323d1491a0b744a
Tatiana <ganzhinovat@yandex.ru>
2022-02-02 11:42:59 +0300

    fix: change code in formPerson.js

--- modules/formPerson.js
+++ modules/formPerson.js
@@ -29,24 +29,27 @@ const createFormPerson = (n) => {
     required: true,
   });
 
+  fieldName.append(labelName, inputName);
+
   const fieldTicket = createElement('div', {
     className: 'field',
   });
 
   const labelTicket = createElement('label', {
     className: 'field__label',
+    for: `ticket${n}`,
     textContent: 'Номер билета (10 цифр)',
   });
 
   const inputTicket = createElement('input', {
     className: 'field__input',
-    id: 'ticket0',
+    id: `ticket${n}`,
     name: 'ticket',
     type: 'text',
     placeholder: 'Номер билета',
     required: true,
-    minLength: '10',
-    maxLength: '10',
+    minLength: 10,
+    maxLength: 10,
   });
 
   const button = createElement('button', {
@@ -55,7 +58,6 @@ const createFormPerson = (n) => {
     textContent: 'Подтвердить',
   });
 
-  fieldName.append(labelName, inputName);
   fieldTicket.append(labelTicket, inputTicket);
   form.append(h2, fieldName, fieldTicket, button);
 
